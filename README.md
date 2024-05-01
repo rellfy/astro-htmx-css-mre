e8bcfcda3cf4c3852e08e1a04a47379bef15a320
Lorenzo Fontoura <lorenzo@nioel.com>
2024-05-02 02:56:19 +1000

    chore: setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# astro + htmx + css modules MRE
+minimal reproducible example for the ASTRO/HTMX use case with CSS modules.
+
+If the Astro dev server does not allow setting `cssCodeSplit: false`, then
+it is not possible to locally develop HTMX apps without global CSS, the only
+alternative would be to preview a production build.
+
+If you know any ways to resolve this, please let me know!
+Otherwise, I believe this
+should be allowed as a setting for the Astro dev server.
+
+For more context:
+1. PR: https://github.com/withastro/astro/pull/10894
+2. Discussion: https://github.com/withastro/roadmap/discussions/909
---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [alpinejs()],
+  adapter: node({
+    mode: "standalone",
+  }),
+  build: {
+    // This is the proposed change which will fix the problem for both
+    // `astro build && astro preview` and `astro dev`.
+    // cssCodeSplit: false,
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 1028,
+      // This gives the correct behaviour if setting it to false, but only
+      // fixes it for `astro build && astro preview` and not for `astro dev`.
+      cssCodeSplit: false
+    },
+  },
+});
